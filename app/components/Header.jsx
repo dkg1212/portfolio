f28e21f82fdbf229bbefeafd6a9afc4ba980d79f
Dimpal Gogoi <dimjkgogoi@gmail.com>
2025-08-19 23:30:19 +0530

    add upto header component

---
+++ app/components/Header.jsx
@@ -0,0 +1,42 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col
+    items-center justify-center gap-4'>
+      <div>
+        <Image src={assets.profile_img} alt='' className='rounded-full w-32 '/>
+       
+      </div>
+       <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'
+        >Hi! I'm ______  <Image src={assets.hand_icon} alt='' 
+        className='w-6'/></h3>
+
+        <h1 className='text-3xl sm:text-6XL lg:text-[66px] font-Ovo'>
+            web developer
+        </h1>
+
+        <p className='max-w-2xl mx-auto font-Ovo'>
+          I'm a passionate web developer with a focus on creating dynamic and 
+          responsive web applications. Let's build something great together!
+        </p>
+
+        <div className='flex flex-col sm:flex-row gap-4 mt-4 items-center'>
+            <a href="#contact" 
+            className='px-10 py-3 border border-full border-gray-500 flex items-center gap-2'
+            >Contact me <Image src={assets.right_arrow_white} alt='' 
+            className='w-4'/></a>
+
+            <a href="/sample-resume.pdf" download
+            className='px-10 py-3 border border-full border-gray-500 flex items-center gap-2'
+            >My Resume <Image src={assets.download_icon} alt='' 
+            className='w-4'/></a>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
