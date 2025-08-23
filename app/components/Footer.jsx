8322a83982394e05f1e80a39b134827d092f238c
Dimpal Gogoi <dimjkgogoi@gmail.com>
2025-08-23 23:00:33 +0530

    add footer page

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,9 +1,34 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className='mt-20'>
+        <div className='text-center '>
+            <Image src={assets.logo} alt='' className='w-36 mx-auto mb-2'/>
+            <div className='w-max flex items-center justify-center gap-2 mx-auto'>
+                <Image src={assets.mail_icon} alt='' className='w-6'/>
+                gogoidimapl546@gmail.com
+            </div>
+        </div>
+        
+        <div className='text-center sm:flex items-center justify-between border-t
+        border-gray-400 mx-[10%] mt-12 py-6'>
+            <p>
+                © 2024 Dimpal Gogoi. All rights reserved.
+            </p>
+            <ul className='flex items-center justify-center gap-10 mt-4 sm:mt-0'>
+                <li><a target='_blank' href='https://github.com/dimpalgogoi'>
+                GitHub</a></li>
+                <li><a target='_blank' href='https://github.com/dimpalgogoi'>
+                LinkedIn</a></li>
+                <li><a target='_blank' href='https://github.com/dimpalgogoi'>
+                Twitter</a></li>
+            </ul>
+        </div>
+
+
     </div>
   )
 }
