1739554e12710ea8413cc6a2ed447315503270fc
Dimpal Gogoi <dimjkgogoi@gmail.com>
2025-08-24 20:52:56 +0530

    add motion effects to Header component for enhanced animations

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,40 +1,69 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import {motion} from 'motion/react'
 
 const Header = () => {
   return (
     <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col
     items-center justify-center gap-4'>
-      <div>
+      <motion.div
+      initial={{scale:0}}
+      whileInView={{scale:1}}
+      transition={{duration:0.8 ,type :'spring' ,stiffness:100}}
+      >
         <Image src={assets.profile_img} alt='' className='rounded-full w-32 '/>
        
-      </div>
-       <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'
+      </motion.div>
+      <motion.h3 
+        initial={{y:-20, opacity:0}}
+        whileInView={{y:0 ,opacity:1}}
+        transition={{duration:0.6 ,delay:0.3}}
+
+        className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'
         >Hi! I'm ______  <Image src={assets.hand_icon} alt='' 
-        className='w-6'/></h3>
+        className='w-6'/></motion.h3>
 
-        <h1 className='text-3xl sm:text-6XL lg:text-[66px] font-Ovo'>
+        <motion.h1 
+        initial={{y:-30, opacity:0}}
+        whileInView={{y:0 ,opacity:1}}
+        transition={{duration:0.8 ,delay:0.5}}
+        className='text-3xl sm:text-6XL lg:text-[66px] font-Ovo'>
             web developer
-        </h1>
+        </motion.h1>
+        
 
-        <p className='max-w-2xl mx-auto font-Ovo'>
+        <motion.p
+        initial={{ opacity:0}}
+        whileInView={{opacity:1}}
+        transition={{duration:0.6 ,delay:0.7}}
+        className='max-w-2xl mx-auto font-Ovo'>
           I'm a passionate web developer with a focus on creating dynamic and 
           responsive web applications. Let's build something great together!
-        </p>
+        </motion.p>
 
         <div className='flex flex-col sm:flex-row gap-4 mt-4 items-center'>
-            <a href="#contact" 
+            <motion.a
+            initial={{y:30, opacity:0}}
+            whileInView={{y:0 ,opacity:1}}
+            transition={{duration:0.6 ,delay:1}} 
+            
+            href="#contact"
             className='px-10 py-3 border border-white rounded-full bg-black 
             text-white flex items-center gap-2 dark:bg-transparent'
             >Contact me <Image src={assets.right_arrow_white} alt='' 
-            className='w-4'/></a>
+            className='w-4'/></motion.a>
 
-            <a href="/sample-resume.pdf" download
+            <motion.a 
+            initial={{y:30, opacity:0}}
+            whileInView={{y:0 ,opacity:1}}
+            transition={{duration:0.6 ,delay:1}} 
+            
+            href="/sample-resume.pdf" download
             className='px-10 py-3 border rounded-full border-gray-500 
             flex items-center gap-2 bg-white dark:text-black'
             >My Resume <Image src={assets.download_icon} alt='' 
-            className='w-4'/></a>
+            className='w-4'/></motion.a>
         </div>
 
     </div>
